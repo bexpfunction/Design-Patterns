7e3a80ccd11dc57023fbe1576c417f0d721ec464
Burak Yazar <burakyazar@hacettepe.edu.tr>
2018-04-26 03:16:57 +0300

    no message

---
+++ PizzaStore(FactoryPattern)/src/pizza/ChicagoStylePizza.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package pizza;
+
+/**
+ * @author Burak Yazar
+ *
+ */
+public class ChicagoStylePizza extends Pizza {
+
+	public ChicagoStylePizza() {
+		name = "Chicago Style";
+		doughType = "Thick";
+		toppings.add("Mosarella Cheese");
+	}
+
+}
